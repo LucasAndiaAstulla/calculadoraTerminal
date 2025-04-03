38aeeb274f5011ddca1fd1700520e8a4b65984f5
LucasAndiaAstulla <tmasterlc.lucas@gmail.com>
2025-04-03 16:05:00 -0300

    CalculadoraTerminal: Quase pronta

--- src/Main.kt
+++ src/Main.kt
@@ -4,15 +4,18 @@ fun main() {
 
     println("Bem vindo!!!")
 
-    do{
+    inicio@ do{
         calculadora()
 
         println("Você deseja continuar?? (SIM/NAO)")
-        val continuar = readLine().orEmpty().uppercase()
+        var continuar = readLine().orEmpty().uppercase()
 
-        //Erro string vazia forma looping
-        while (!continuar.equals("NÃO", ignoreCase = true) && !continuar.equals("SIM", ignoreCase = true)) {
+        //Tratamento se o valor for != SIM ou NAO
+
+        while (continuar.isBlank() || !continuar.equals("NÃO", ignoreCase = true) && !continuar.equals("SIM", ignoreCase = true)) {
             println("Por favor insira somente SIM ou NÃO!!!")
+            continuar = readLine().orEmpty().uppercase()
+
         }
 
     } while (continuar == "SIM")
@@ -20,25 +23,31 @@ fun main() {
 }
 
 fun calculadora() {
+
+
     println("Escolha a quantidade de variáveis a se efetuar:")
-    var quantVar = readLine()?.toIntOrNull() ?: 0
-    val valores = mutableListOf<Int>()
+    var quantVar = readLine()?.toDoubleOrNull() ?: 0.0
+    val valores = mutableListOf<Double>()
 
     while(quantVar <= 0) {
         println("Foi confirmado uma variável errada, tente de novo!!")
-        quantVar = readLine()?.toIntOrNull() ?: 0
+        quantVar = readLine()?.toDoubleOrNull() ?: 0.0
     }
 
-    for (i in 1..quantVar) {
+    for (i in 1..quantVar.toInt()) {
 
-        var valor: Int?
+        var valor: Double?
 
         do {
             println("Digite o valor $i: ")
-            valor = readLine()?.toIntOrNull()
+            valor = readLine()?.toDoubleOrNull()
+
+
             if (valor == null) {
                 println("Entrada inválida! Digite apenas números inteiros.")
             }
+
+
         } while(valor == null) //valor incorreto, faz permanecer no loop
 
         //adiciona no array
@@ -64,6 +73,8 @@ fun calculadora() {
                     "(*) - multiplicação\n"
         )
         operador = readLine().orEmpty()
+
+
     } while (operador !in permitidos)
 
     println("Muito obrigado, prosseguiremos...")
@@ -72,7 +83,7 @@ fun calculadora() {
     println(calcularResultado(operador, valores))
 }
 
-fun calcularResultado(operador: String?, valores: List<Int>): String {
+fun calcularResultado(operador: String?, valores: List<Double>): String {
 
     return when(operador) {
         "+" ->  "O resultado é ${valores.sum()}"
@@ -80,7 +91,7 @@ fun calcularResultado(operador: String?, valores: List<Int>): String {
         "*" ->  "O resultado é ${valores.reduce{acc, num -> acc * num}}"
         "/" -> {
             //valores.qualquerum { é = 0 ou primeiro = 0}
-            if(valores.any{ it == 0} || valores.first() == 0) return "Erro de divisão por zero!"
+            if(valores.any{ it == 0.0} || valores.first() == 0.0) return "Erro de divisão por zero!"
             "O resultado é ${valores.reduce { acc, num -> acc / num }}"
         }
         else -> "Operador invalido"
